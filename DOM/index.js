e9375c334779cee70b9e1ebd9450bab4eb33c9e3
akash <akashtidke2014@gmail.com>
2022-03-04 01:03:59 +0530

    added

---
+++ DOM/index.js
@@ -0,0 +1,3 @@
+alert("hello")
+var value=document.querySelector("h1");
+var change=value.innerHTML="me";
---
+++ javaScript/lve calculator/index.js
@@ -0,0 +1,5 @@
+var urname="";
+var crname="";
+var num=Math.random();
+var ansNum=(num*100)+1;
+console.log(Math.floor(ansNum));
